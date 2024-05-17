bb96d79286ef4b80e65f7e5ae46ed6fc971abbdd
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-05-17 20:19:06 +0300

    best selles list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookia/Core/Utils/styles.dart';
+import 'package:bookia/Core/assets.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -14,11 +15,39 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBooksListView(),
         SizedBox(height: 40),
-        Text(
-          'Best Seller',
-          style: Styles.titleMedium,
-        )
+        Text('Best Seller', style: Styles.titleMedium),
+        BestSellerListViewItem()
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.bookImage),
+                ),
+              ),
+            ),
+          ),
+          const Column(
+            children: [],
+          )
+        ],
+      ),
     );
   }
 }
