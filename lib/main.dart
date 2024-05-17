2c070cc584fa8cae1145bde6794838850dd922b6
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-05-17 20:07:31 +0300

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookia/Features/Splash/Presentation/Views/splash_view.dart';
 import 'package:bookia/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Bookia());
@@ -17,6 +18,9 @@ class Bookia extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+        ),
       ),
       home: const SplashView(),
     );
