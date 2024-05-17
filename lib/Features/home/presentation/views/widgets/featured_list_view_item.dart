bec2bdc54fd439ed0e0863eac0e89a1efe233ee4
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-05-17 15:10:57 +0300

    create style file and add title to home view

--- lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,8 +1,8 @@
 import 'package:bookia/Core/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({
     super.key,
   });
 
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,7 @@
-import 'package:bookia/Features/home/presentation/views/widgets/Featured_list_view.dart';
+import 'package:bookia/Core/Utils/styles.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookia/Features/home/presentation/views/widgets/custom_list_view_item.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:bookia/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,7 +9,16 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(), FeaturedBooksListView()],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(),
+        FeaturedBooksListView(),
+        SizedBox(height: 40),
+        Text(
+          'Best Seller',
+          style: Styles.titleMedium,
+        )
+      ],
     );
   }
 }
