39caf2ca4adef02ec0c80aab22a7c6ab72643b6e
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-05-20 08:53:34 +0300

    best seller view par 1

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -3,21 +3,26 @@ import 'package:bookia/Core/assets.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        SizedBox(height: 40),
-        Text('Best Seller', style: Styles.textStyle18),
-        BestSellerListViewItem()
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 40),
+          Text('Best Seller', style: Styles.textStyle18),
+          SizedBox(height: 20),
+          BestSellerListViewItem()
+        ],
+      ),
     );
   }
 }
@@ -43,8 +48,19 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
-          const Column(
-            children: [],
+          const SizedBox(width: 30),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ],
           )
         ],
       ),
