485bb9ccb7ed20644b53fb16b42a55d961be6c0f
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-05-21 11:11:04 +0300

    best seller item part 2

---
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,62 @@
+import 'package:bookia/Core/Utils/styles.dart';
+import 'package:bookia/Core/assets.dart';
+import 'package:bookia/constants.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.bookImage),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text(
+                'J.K. Rpwling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(height: 3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '19.99 \$',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookia/Core/Utils/styles.dart';
-import 'package:bookia/Core/assets.dart';
+import 'package:bookia/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookia/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -21,47 +21,6 @@ class HomeViewBody extends StatelessWidget {
           Text('Best Seller', style: Styles.textStyle18),
           SizedBox(height: 20),
           BestSellerListViewItem()
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.bookImage),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
