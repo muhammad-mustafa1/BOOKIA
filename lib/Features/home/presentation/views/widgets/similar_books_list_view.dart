3ed9c02e59f4fa6719a4db255d5d673a5a2f5197
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-06-02 02:08:34 +0300

    create similar books list view

---
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:bookia/Features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .15,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        separatorBuilder: (context, index) => const SizedBox(width: 10),
+        itemBuilder: (context, index) => const CustomBookImage(),
+      ),
+    );
+  }
+}
