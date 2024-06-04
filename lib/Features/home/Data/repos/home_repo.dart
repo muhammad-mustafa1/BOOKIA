4486cd16def44c7632e20207ea3069cdd23c959c
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-06-04 11:53:37 +0300

    create home repo methods and failure file

--- lib/Features/home/Data/repos/home_repo.dart
+++ lib/Features/home/Data/repos/home_repo.dart
@@ -1,5 +1,10 @@
-// كلاس يتم به تحديد الميثود التي يتم تنفيذها
+// كلاس يتم به تحديد الميثود التي يتم تنفيذها HomeRepo
+// Either لانشاء ميثود ترجع اكثر من نوع
+import 'package:bookia/Core/errors/failures.dart';
+import 'package:bookia/Features/home/Data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
