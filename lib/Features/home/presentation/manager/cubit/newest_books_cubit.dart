777eb25397d4e143b6d43cd95f512204991d705a
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-06-08 06:37:00 +0300

    newest books cubit done

--- lib/Features/home/presentation/manager/cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/cubit/newest_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookia/Features/home/Data/models/book_model/book_model.dart';
+import 'package:bookia/Features/home/Data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) {
+        emit(
+          NewestBooksFailure(errorMessage: failure.errorMessage),
+        );
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
 }
