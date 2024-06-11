f646b6ff39721b7e1a1495180e1cfdb1a6a58d33
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2024-06-12 01:12:31 +0300

    implement fetch similler books

--- lib/Features/home/Data/repos/home_repo.dart
+++ lib/Features/home/Data/repos/home_repo.dart
@@ -7,4 +7,6 @@ import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimillerBooks(
+      {required String category});
 }
--- lib/Features/home/Data/repos/home_repo_impl.dart
+++ lib/Features/home/Data/repos/home_repo_impl.dart
@@ -49,4 +49,27 @@ class HomeRepoImpl implements HomeRepo {
       }
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimillerBooks({
+    required String category,
+  }) async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting:relevance&q=subject:computer scince');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
+  }
 }
